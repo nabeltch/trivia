f726a1e5ca80d9b31e060a260b518b03cbb3b6c0
nabeltch <106460291+nabeltch@users.noreply.github.com>
2022-09-16 19:10:27 -0500

    Create data_trivia.py

---
+++ data_trivia.py
@@ -0,0 +1,79 @@
+trivia = {
+    'question 1': {
+        'question': '¿Quién fue el creador de windows?',
+        'options': {
+            'a': 'Linus Torvalds',
+            'b': 'Bill Gates',
+            'c': 'Mark Zuckerberg',
+            'd': 'Dennis Ritchie'
+        },
+        'response': 'b',
+        'feeback': True,
+        'items_feeback': {
+            'a': 'Incorrecto, Linus Torvalds fue fundador de Linux',
+            'b':
+            'Correcto, Bill Gates fue el creador de Windows, Vamos sigue adelante !!',
+            'c': 'Incorrecto, Mark Zuckerberg fue fundador de Facebook',
+            'd': 'Incorrecto, Dennis Ritchie fue fundador del lenjuaje C'
+        },
+        'score': {
+            'a': '/2',
+            'b': '*2',
+            'c': '-5',
+            'd': '+5'
+        }
+    },
+    'question 2': {
+        'question':
+        '¿Cuál de estos lenguajes de programación es de más bajo nivel?',
+        'options': {
+            'a': 'Python',
+            'b': 'Java',
+            'c': 'PHP',
+            'd': 'Assembly'
+        },
+        'response': 'd',
+        'feeback': True,
+        'items_feeback': {
+            'a':
+            'Incorrecto, el lenjuaje python es interpretado y es de alto nivel',
+            'b':
+            'Incorrecto, el lenjuaje java es compilado y es de alto nivel',
+            'c':
+            'Incorrecto, el lenjuaje php es interpretado y es de alto nivel',
+            'd':
+            'Correcto, el lenjuaje Assembly es de bajo nivel, Vamos sigue adelante !!'
+        },
+        'score': {
+            'a': '/2',
+            'b': '+5',
+            'c': '-5',
+            'd': '*2'
+        }
+    },
+    'question 3': {
+        'question': '¿En qué lenguaje se programó Minecraft?',
+        'options': {
+            'a': 'Python',
+            'b': 'Java',
+            'c': 'PHP',
+            'd': 'Assembly'
+        },
+        'response': 'b',
+        'feeback': True,
+        'items_feeback': {
+            'a': 'Incorrecto, Minecraft no se programó en el lenjuaje python',
+            'b':
+            'Correcto, Minecraft se programó en el lenjuaje Java, Vamos sigue adelante !!',
+            'c': 'Incorrecto, Minecraft no se programó en el lenjuaje php',
+            'd':
+            'Incorrecto, Minecraft no se programó en el lenjuaje assembly',
+        },
+        'score': {
+            'a': '/2',
+            'b': '*2',
+            'c': '-5',
+            'd': '+5'
+        }
+    }
+}
